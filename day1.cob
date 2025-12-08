c8cc18258f7b642c3fad8ef02054881b3b727a33
Novitz Jean-Philippe <novitz@gmail.com>
2025-12-08 14:08:02 +0100

    Day 1 : Hello world

---
+++ day1.cob
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. day1.
+       AUTHOR. jphNovitz.
+       DATE-WRITTEN. 12/09/2025.
+       PROCEDURE DIVISION.
+           display "Hello world".
+           STOP RUN.
