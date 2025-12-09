19a4fc086d53c738c093e09d6333dad17c3bc059
Novitz Jean-Philippe <novitz@gmail.com>
2025-12-09 12:45:44 +0100

    Day 2 : Data Division

---
+++ day2.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. day1.
+       AUTHOR. jphNovitz.
+       DATE-WRITTEN. 12/09/2025.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 My-name PIC X(15) VALUE "Jean-Philippe".
+       01 My-age PIC 9(2) VALUE 52.
+
+       PROCEDURE DIVISION.
+           display My-name.
+           display My-age.
+           STOP RUN.
